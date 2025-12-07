5f6b02776b31d8464ded17c6f0c9445c69234a6c
nurulamin092 <mnurulamin092@gmail.com>
2025-12-07 08:29:03 +0600

    create vehicle table

--- src/config/database.ts
+++ src/config/database.ts
@@ -22,6 +22,18 @@ const initDb = async () => {
   );
 
   await pool.query(`
+  CREATE TABLE IF NOT EXISTS vehicles(
+  id SERIAL PRIMARY KEY,
+  vehicle_name VARCHAR(150) NOT NULL,
+  type VARCHAR(50) NOT NULL CHECK(type IN('car','bike','van','SUV')),
+  registration_number VARCHAR (150) UNIQUE NOT NULL,
+  daily_rent_price NUMERIC(10,2) NOT NULL CHECK (daily_rent_price>0),
+  availability_status VARCHAR(100) NOT NULL CHECK(availability_status IN('available','booked')),
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW())  
+ `);
+
+  await pool.query(`
     CREATE UNIQUE INDEX IF NOT EXISTS users_email_lower_idx ON users (LOWER(email))
     `);
 };
